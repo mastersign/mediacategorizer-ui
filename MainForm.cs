d2f267b9fb551a77f3f9a2e34b906b577e791f53
mastersign <dev@mastersign.de>
2013-09-26 11:24:55 +0200

    import empty project

---
+++ MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace de.fhb.oll.mediacategorizer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
