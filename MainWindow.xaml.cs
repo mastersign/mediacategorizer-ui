15f3bf786093f839ca934ff2eae59242f205042c
mastersign <dev@mastersign.de>
2013-09-27 14:15:36 +0200

    implemented basics of framenavigation and workflow visualization

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace de.fhb.oll.mediacategorizer
 {
@@ -14,6 +16,33 @@ namespace de.fhb.oll.mediacategorizer
         public MainWindow()
         {
             InitializeComponent();
+            MyNavigate("Start");
+        }
+
+        private void MyNavigate(string page)
+        {
+            frame.Navigate(new Uri("Page" + page + ".xaml", UriKind.Relative));
+        }
+
+        private void ExpanderExpandedHandler(object sender, RoutedEventArgs e)
+        {
+            var expander = (Expander)e.Source;
+            foreach (var exp in navigationPanel.Children.OfType<Expander>().Where(exp => exp != expander))
+            {
+                exp.IsExpanded = false;
+            }
+            var nextPage = ((string)expander.Tag);
+            MyNavigate(nextPage);
+        }
+
+        private void MenuNewProjectHandler(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "NewProject", "Menu", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MenuOpenProjectHandler(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "OpenProject", "Menu", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
