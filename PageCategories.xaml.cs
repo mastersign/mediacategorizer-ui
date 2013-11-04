da350cf3404f131d84e3b6a2f13c7b2a9b88bdf0
mastersign <dev@mastersign.de>
2013-11-04 10:25:15 +0100

    implemented adding and removing categories

--- PageCategories.xaml.cs
+++ PageCategories.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using de.fhb.oll.mediacategorizer.model;
 
 namespace de.fhb.oll.mediacategorizer
 {
@@ -23,6 +24,28 @@ namespace de.fhb.oll.mediacategorizer
         public PageCategories()
         {
             InitializeComponent();
+        }
+
+        private Project ProjectModel {get { return DataContext as Project; }}
+
+        private Category SelectedCategory {get { return categoryDataGrid.SelectedItem as Category; }}
+
+        private void CreateCategoryHandler(object sender, RoutedEventArgs e)
+        {
+            if (ProjectModel == null) return;
+            ProjectModel.Categories.Add(new Category());
+        }
+
+        private void DeleteCategoryHandler(object sender, RoutedEventArgs e)
+        {
+            if (ProjectModel == null) return;
+            if (SelectedCategory == null) return;
+            ProjectModel.Categories.Remove(SelectedCategory);
+        }
+
+        private void CategorySelectionChangedHandler(object sender, RoutedEventArgs e)
+        {
+            btnDeleteCategory.IsEnabled = SelectedCategory != null;
         }
     }
 }
