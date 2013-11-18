155ad050040765ef4d42691ec282ea1c4ccd70ac
mastersign <dev@mastersign.de>
2013-11-18 16:35:12 +0100

    added more demo data

--- model/Project.cs
+++ model/Project.cs
@@ -41,34 +41,75 @@ namespace de.fhb.oll.mediacategorizer.model
             Description = "Ein Testprojekt zum Testen.";
             OutputDir = @"C:\Temp\MediaCategorizerOutput";
 
+            #region Categories
+
             Categories.Add(new Category
             {
-                Id = "tarnung",
-                Name = "Camouflage",
+                Id = "info",
+                Name = "Informatik",
                 Resources = new ObservableCollection<CategoryResource>
                 {
-                    new CategoryResource {Type = CategoryResourceType.Wikipedia, Url = "http://de.wikipedia.org/wiki/Tarnung"},
-                    new CategoryResource {Type = CategoryResourceType.Html, Url = "http://www.duden.de/rechtschreibung/Tarnung"},
+                    new CategoryResource {Type = CategoryResourceType.Wikipedia, Url = "http://de.wikipedia.org/wiki/Informatik"},
+                    new CategoryResource {Type = CategoryResourceType.Wikipedia, Url = "http://de.wikipedia.org/wiki/Theoretische_Informatik"},
+                    new CategoryResource {Type = CategoryResourceType.Wikipedia, Url = "http://de.wikipedia.org/wiki/Praktische_Informatik"},
+                    new CategoryResource {Type = CategoryResourceType.Wikipedia, Url = "http://de.wikipedia.org/wiki/Technische_Informatik"},
                 },
             });
             Categories.Add(new Category
             {
-                Id = "bird",
-                Name = "Vogel",
+                Id = "queue",
+                Name = "Datenstrukturen - Warteschlange",
                 Resources = new ObservableCollection<CategoryResource>
                 {
-                    new CategoryResource
-                    {
-                        Type = CategoryResourceType.Html,
-                        Url = "http://www.ausgabe.natur-lexikon.com/Voegel.php"
-                    },
-                    new CategoryResource
-                    {
-                        Type = CategoryResourceType.Wikipedia,
-                        Url = "http://de.wikipedia.org/wiki/V%C3%B6gel"
-                    },
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Warteschlange_%28Datenstruktur%29"},
                 },
             });
+            Categories.Add(new Category
+            {
+                Id = "stack",
+                Name = "Datenstrukturen - Stapel",
+                Resources = new ObservableCollection<CategoryResource>
+                {
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Stapelspeicher"},
+                },
+            });
+            Categories.Add(new Category
+            {
+                Id = "tree",
+                Name = "Datenstrukturen - Baum",
+                Resources = new ObservableCollection<CategoryResource>
+                {
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Baum_%28Graphentheorie%29"},
+                },
+            });
+            Categories.Add(new Category
+            {
+                Id = "math",
+                Name = "Mathematik",
+                Resources = new ObservableCollection<CategoryResource>
+                {
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Mathematik"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Rechnen"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Zahl"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Arithmetik"},
+                },
+            });
+            Categories.Add(new Category
+            {
+                Id = "oeko",
+                Name = "Wirtschaft",
+                Resources = new ObservableCollection<CategoryResource>
+                {
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Wirtschaft"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Wirtschaftswissenschaft"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Bank"},
+                    new CategoryResource{Type = CategoryResourceType.Wikipedia,Url = "http://de.wikipedia.org/wiki/Geld"},
+                },
+            });
+
+            #endregion
+
+            #region Media
 
             Media.Add(new Media
             {
@@ -78,19 +119,89 @@ namespace de.fhb.oll.mediacategorizer.model
             });
             Media.Add(new Media
             {
+                Id = "12-01-1-Datenstrukturen",
+                Name = "12.01.1 Datenstrukturen, Array, Queue, Stack",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\12.01.1 Datenstrukturen, Array, Queue, Stack.mp4"),
+            });
+            Media.Add(new Media
+            {
                 Id = "12-01-2-Baum",
                 Name = "12.01.2 Baum als Datenstruktur",
                 MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\12.01.2 Baum als Datenstruktur.mp4"),
             });
+            Media.Add(new Media
+            {
+                Id = "Algo1",
+                Name = "Algorithmen und Datenstrukturen 001",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Algorithmen und Datenstrukturen 001.mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Algo2",
+                Name = "Algorithmen und Datenstrukturen 002",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Algorithmen und Datenstrukturen 002.mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Algo3",
+                Name = "Algorithmen und Datenstrukturen 003",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Algorithmen und Datenstrukturen 003.mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Algo4",
+                Name = "Algorithmen und Datenstrukturen 004",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Algorithmen und Datenstrukturen 004.mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Bernoulli",
+                Name = "Binomialverteilung_Formel von Bernoulli, Stochastik, Nachhilfe online, Hilfe in Mathe",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Binomialverteilung_Formel von Bernoulli, Stochastik, Nachhilfe online, Hilfe in Mathe (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Hypothesentest",
+                Name = "Einseitiger(rechtsseitiger) Hypothesentest_mit Ablesen aus der Tabelle, Stochastik, Nachhilfe online",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Einseitiger(rechtsseitiger) Hypothesentest_mit Ablesen aus der Tabelle, Stochastik, Nachhilfe online (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Parabeln",
+                Name = "Parabeln_Quadratische Funktion_en Übersicht (Scheitelpunkt,Stauchung,Streckung,etc.)",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Parabeln_Quadratische Funktion_en Übersicht (Scheitelpunkt,Stauchung,Streckung,etc.) (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Gauss",
+                Name = "Gauß-Algorithmus_Lineares Gleichungssystem lösen (einfach_schnell erklärt), Nachhilfe online",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Gauß-Algorithmus_Lineares Gleichungssystem lösen (einfach_schnell erklärt), Nachhilfe online (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Lambda",
+                Name = "Der Lambda-Kalkül",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Der Lambda-Kalkül (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "IS-Kurve",
+                Name = "IS-Kurve im Vier-Quadrantenschema Die Herleitung",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\IS-Kurve im Vier-Quadrantenschema Die Herleitung (720).mp4"),
+            });
+            Media.Add(new Media
+            {
+                Id = "Makrooekonomie",
+                Name = "Makroökonomie online lernen - VWL Tutorium",
+                MediaFile = Path.Combine(basePath, @"Media\Video\de-DE\Makroökonomie online lernen - VWL Tutorium (360).mp4"),
+            });
 
-            Configuration.MainCloud.FontFamily = new FontFamily("Arial");
-            Configuration.CategoryCloud.FontFamily = new FontFamily("Arial");
-            Configuration.MediaCloud.FontFamily = new FontFamily("Arial");
+            #endregion
+
+            Configuration.MainCloud.FontFamily = new FontFamily("Segoe UI");
+            Configuration.CategoryCloud.FontFamily = new FontFamily("Segoe UI");
+            Configuration.MediaCloud.FontFamily = new FontFamily("Segoe UI");
         }
 
-        public string GetWorkingDirectory()
-        {
-            return Path.Combine(OutputDir, WORKING_DIR_NAME);
-        }
     }
 }
